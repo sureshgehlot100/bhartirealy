e631beb00ce828d2a7e15594fe19915d1e247e66
sureshgehlot100 <sureshgehlot7860@gmail.com>
2024-10-24 14:08:08 +0530

    logo

--- next/src/app/component/Navbar.js
+++ next/src/app/component/Navbar.js
@@ -1,44 +1,37 @@
 "use client"
 import { Menu, X } from 'lucide-react';
+import Image from 'next/image';
 import Link from 'next/link';
 import { useState } from 'react';
 
 export default function Navbar() {
     const [isOpen, setIsOpen] = useState(false)
     const toggleMenu = () => setIsOpen(!isOpen)
-  
+
     const menuItems = [
-      'Rent',
-      'Buy',
-      'Sell',
-      'Manage Property',
-      'Resources',
-      'LogIn',
-      'LogOut'
+        'Rent',
+        'Buy',
+        'Sell',
+        'Manage Property',
+        'Resources',
+        'LogIn',
+        'LogOut'
     ]
     return (
         <nav className="bg-[#fafafe] shadow-md fixed top-0 left-0 w-full z-30 ">
             <div className="max-w-7xl mx-auto px-4 py-4 flex justify-between items-center">
                 <div className="text-2xl font-bold text-[black]">
                     <div className='flex'>
-                        <svg
-                            width={35}
-                            height={30}
+                        <Image
+                            width={200}
+                            height={100}
                             viewBox="0 0 26 26"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="w-[31.33px] h-[25.35px] mx-2 relative mt-1"
+                            src="/logor.png"
+                            className=" mx-2 relative mt-1"
                             preserveAspectRatio="none"
                         >
-                            <path
-                                d="M22.3333 8.61331L15.2226 3.08264C14.5986 2.59719 13.8306 2.33362 13.04 2.33362C12.2494 2.33362 11.4813 2.59719 10.8573 3.08264L3.74529 8.61331C3.31788 8.94569 2.97208 9.37135 2.7343 9.85778C2.49651 10.3442 2.37304 10.8785 2.37329 11.42V21.02C2.37329 21.7272 2.65424 22.4055 3.15434 22.9056C3.65444 23.4057 4.33271 23.6866 5.03996 23.6866H21.04C21.7472 23.6866 22.4255 23.4057 22.9256 22.9056C23.4257 22.4055 23.7066 21.7272 23.7066 21.02V11.42C23.7066 10.3226 23.2 9.28664 22.3333 8.61331Z"
-                                fill="#7065F0"
-                                stroke="#7065F0"
-                                strokeWidth={3}
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                            />
-                        </svg>Bharti Realty</div>
+
+                        </Image></div>
                 </div>
                 <div className="hidden md:flex flex-grow justify-center space-x-6">
                     <Link href="/Rent" className="text-gray-600 hover:text-blue-600">Rent</Link>
